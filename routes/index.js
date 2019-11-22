1b081b3224e655d1c80fddfe37fc6c6f5f5c45f8
mlglz <mlglz@163.com>
2019-11-22 15:33:56 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+/* GET home page. */
+router.get('/',(req,res)=>{
+  res.send('Hello Express')
+})
+
+router.get('/about',(req,res)=>{
+  res.send('about me ?  我是个有肌肉男!')
+})
+
+/**
+ * 上传
+ */
+router.post('/upload', upload.single('file'), function (req, res, next) {
+   console.log(req.file)
+   res.send('ok')
+})
+
+module.exports = router;
