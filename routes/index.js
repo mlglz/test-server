e843f2d6748d8d2bba1c099babcc03351b7f5949
mlglz <mlglz@163.com>
2019-11-28 10:04:09 +0800

    忽略uploads

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+
+
+//配置multer保存扩展名
+var storge = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, '/uploads')
+  },
+  filename: function (req, file, cb) {
+      var fileformat = (file.originalname).split('.');
+      cb(null, file.fieldname+'-'+Date.now()+'.'+fileformat[fileformat.length-1]);
+  }
+})
+
+//使用配置
+var upload = multer({storage: storge})
 
 /* GET home page. */
 router.get('/',(req,res)=>{
@@ -9,23 +23,28 @@ router.get('/',(req,res)=>{
 })
 
 router.get('/about',(req,res)=>{
-  res.send('about me ?  我是个有肌肉男!')
+  res.send('about me ?  我是个肌肉男!')
 })
 
 /**
  * 上传
  */
 router.post('/upload', upload.single('file'), function (req, res, next) {
-   console.log(req.file)
+  //  console.log(req.file)
+   console.log('upload')
    res.send('ok')
 })
 
 /**
- * 读取文件
+ * 上传多个
  */
-router.get('/upload',(req,res)=>{
-  res.send(req.url)
+router.post('/uploadm', upload.array('photo' , 3), function (req, res, next) {
+   console.log(req.files)
+   console.log('upload')
+   res.send('ok')
 })
+
+
 
 
 module.exports = router;
